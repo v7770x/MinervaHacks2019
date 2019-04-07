e4307db53fb2613051b077ec6e2be2c8f30e3680
Vishvam Mazumdar <vmazumda@vishvam-inspiron-ubuntu-18.04>
2019-04-06 21:23:44 -0700

    fixed bug

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-# print(db.child("Hosts").get().val())
+print(db.child("Hosts").get().val())
 
 @app.route("/get_matches")
 def get_matches(location, num_ppl, start_date, duration):
 	possible_hosts = db.child("Hosts").get().val()
 	matches = []
 	for host in possible_hosts:
-		print(host, possible_hosts[host])
+		# print(host, possible_hosts[host])
 		host_features = possible_hosts[host]
 		if(host_features.get("location")==location and host_features.get("num_ppl")>=num_ppl and host_features.get("duration")>=duration):
 			date = host_features.get("start_date")
@@ -35,7 +35,7 @@ def get_matches(location, num_ppl, start_date, duration):
 
 # def hi
 
-get_matches("San Francisco",3,10,6)
+get_matches("San Francisco",3,{"month": 9, "year":2019, "day":17},6)
 
 
 @app.route("/")
