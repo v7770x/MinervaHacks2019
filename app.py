654828e802abf1efead362fe0bcd69343290f44a
Vishvam Mazumdar <vmazumda@vishvam-inspiron-ubuntu-18.04>
2019-04-06 19:47:23 -0700

    added firebase integration and match filtering

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+
+
+import pyrebase
+
 app = Flask(__name__)
+CORS(app)
+
+
+config = {
+  "apiKey": "AIzaSyBep4hN7RY5OSQw7SmxYw1S62CtYUaZtek",
+  "authDomain": "minerva-hacks-19.firebaseapp.com",
+  "databaseURL": "https://minerva-hacks-19.firebaseio.com/",
+  "storageBucket": "minerva-hacks-19.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+print(db.child("Hosts").get().val())
+
+@app.route("/get_matches")
+def get_matches(location, num_ppl, start_date, duration):
+	possible_hosts = db.child("Hosts").get().val()
+	matches = []
+	for host in possible_hosts:
+		print(host, possible_hosts[host])
+		host_features = possible_hosts[host]
+		if(host_features.get("location")==location and host_features.get("num_ppl")>=num_ppl and host_features.get("duration")>=duration and
+			 abs(host_features["start_date"] - start_date)<5 ):
+			matches.append(host_features)
+	print(matches)
+	# return jsonify(matches)
+
+# def hi
+
+get_matches("San Francisco",3,10,6)
 
 
 @app.route("/")
 def main_page():
 	return render_template("index.html")
+
+
